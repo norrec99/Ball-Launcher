a81ca00142a2c1274fd1746a7b29cec59ae18b53
cagataysoyer <cagatay@stardust.gs>
2023-07-29 15:09:41 +0300

    Launching the ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,15 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachDelay = 0.1f;
     private Camera mainCamera;
 
+    private bool isDragging;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         mainCamera = Camera.main;       
     }
@@ -16,15 +22,41 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (currentBallRigidbody == null) { return; }
+
         if (!Touchscreen.current.press.isPressed)
         {
+            if (isDragging)
+            {
+                LauncBall();
+                isDragging = false;
+            }
             return;
         }
+
+        isDragging = true;
+        currentBallRigidbody.isKinematic = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log("worldPosition " + worldPosition);
+        currentBallRigidbody.position = worldPosition;
+
+
+    }
+
+    private void LauncBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
