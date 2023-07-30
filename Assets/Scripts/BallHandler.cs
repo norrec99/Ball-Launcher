efa3469539299ffb9db4743daa3521368df79244
cagataysoyer <cagatay@stardust.gs>
2023-07-30 13:06:44 +0300

    add multitouch for test purpose

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class BallHandler : MonoBehaviour
 {
@@ -28,12 +30,22 @@ public class BallHandler : MonoBehaviour
         SpawnBall();
     }
 
+    private void OnEnable() 
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable() 
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (currentBallRigidbody == null) { return; }
 
-        if (!Touchscreen.current.press.isPressed)
+        if (Touch.activeTouches.Count == 0)
         {
             if (isDragging)
             {
@@ -46,13 +58,18 @@ public class BallHandler : MonoBehaviour
         isDragging = true;
         currentBallRigidbody.isKinematic = true;
 
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        Vector2 touchPosition = new Vector2();
+
+        foreach (Touch touch in Touch.activeTouches)
+        {
+            touchPosition += touch.screenPosition;
+        }
+
+        touchPosition /= Touch.activeTouches.Count;
 
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
         currentBallRigidbody.position = worldPosition;
-
-
     }
 
     private void SpawnBall()
